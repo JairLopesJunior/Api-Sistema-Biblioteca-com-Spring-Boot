28d6096f07f58e3ed1bbd0c0a34f988ca482aea1
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-05-04 23:20:11 -0300

    Criado projeto Spring com as dependencias do Lombok, Web e DevTools

---
+++ sistemabibliotecario/src/main/java/io/github/jairlopesjunior/sistemabibliotecario/SistemabibliotecarioApplication.java
@@ -0,0 +1,13 @@
+package io.github.jairlopesjunior.sistemabibliotecario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemabibliotecarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemabibliotecarioApplication.class, args);
+	}
+
+}
---
+++ sistemabibliotecario/src/test/java/io/github/jairlopesjunior/sistemabibliotecario/SistemabibliotecarioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jairlopesjunior.sistemabibliotecario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemabibliotecarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
