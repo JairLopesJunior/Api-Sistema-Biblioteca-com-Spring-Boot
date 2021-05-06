303faf399a7b7f61d1c4369d2af77eb40cc1d009
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-05-05 23:37:42 -0300

    Criado a Entidade Cliente

---
+++ sistemabibliotecario/src/main/java/io/github/jairlopesjunior/sistemabibliotecario/domain/entities/Cliente.java
@@ -0,0 +1,37 @@
+package io.github.jairlopesjunior.sistemabibliotecario.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nome", length = 100)
+    @NotEmpty(message = "Campo nome é obrigatório.")
+    private String nome;
+
+    @Column(name = "cpf", length = 11)
+    @CPF(message = "Campo cpf é obrigatório.")
+    private String cpf;
+
+    @Column(name = "email", length = 100)
+    @Email(message = "Campo email é obrigatório.")
+    private String email;
+}
