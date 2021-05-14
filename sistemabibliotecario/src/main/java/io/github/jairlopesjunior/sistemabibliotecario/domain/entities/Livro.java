c0969e663ee144098c06529fc098d51785ca5b21
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-05-14 19:01:32 -0300

    Populado a Classe LivroDTO e movido as validações da Entity para o DTO

--- sistemabibliotecario/src/main/java/io/github/jairlopesjunior/sistemabibliotecario/domain/entities/Livro.java
+++ sistemabibliotecario/src/main/java/io/github/jairlopesjunior/sistemabibliotecario/domain/entities/Livro.java
@@ -5,16 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Getter
@@ -31,30 +22,24 @@ public class Livro {
     private Integer id;
 
     @Column(name = "nome_livro")
-    @NotEmpty(message = "Campo nome do livro é obrigatório.")
     private String nomeLivro;
 
     @Column(name = "autor")
-    @NotEmpty(message = "Campo autor é obrigatório.")
     private String autor;
 
     @Column(name = "ano_livro")
     private LocalDate anoLivro;
 
     @Column(name = "genero")
-    @NotEmpty(message = "Campo genero é obrigatório.")
     private String genero;
 
     @Column(name = "editora")
-    @NotEmpty(message = "Campo editora é obrigatório.")
     private String editora;
 
     @Column(name = "pagina")
-    @NotNull(message = "Campo pagina é obrigatório.")
     private Integer pagina;
 
     @Column(name = "status")
-    @NotEmpty(message = "Campo status é obrigatório.")
     private String status;
 
     @ManyToOne
--- sistemabibliotecario/src/main/java/io/github/jairlopesjunior/sistemabibliotecario/rest/dtos/LivroDTO.java
+++ sistemabibliotecario/src/main/java/io/github/jairlopesjunior/sistemabibliotecario/rest/dtos/LivroDTO.java
@@ -1,13 +1,38 @@
 package io.github.jairlopesjunior.sistemabibliotecario.rest.dtos;
 
+import io.github.jairlopesjunior.sistemabibliotecario.domain.entities.Cliente;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class LivroDTO {
 
+    @NotEmpty(message = "Campo nome do livro é obrigatório.")
     private String nomeLivro;
+
+    @NotEmpty(message = "Campo autor é obrigatório.")
+    private String autor;
+
+    private LocalDate anoLivro;
+
+    @NotEmpty(message = "Campo genero é obrigatório.")
+    private String genero;
+
+    @NotEmpty(message = "Campo editora é obrigatório.")
+    private String editora;
+
+    @NotNull(message = "Campo pagina é obrigatório.")
+    private Integer pagina;
+
+    @NotEmpty(message = "Campo status é obrigatório.")
+    private String status;
+
+    private Cliente cliente;
 }
