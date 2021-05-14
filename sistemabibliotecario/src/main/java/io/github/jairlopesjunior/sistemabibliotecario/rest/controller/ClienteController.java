075546275aa5ad7f701e240ee0b8cc37fd4d8598
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-05-13 21:11:33 -0300

    Adicionado as dependencias do Swagger e mapeado com @ApiOperation

--- sistemabibliotecario/src/main/java/io/github/jairlopesjunior/sistemabibliotecario/rest/controller/ClienteController.java
+++ sistemabibliotecario/src/main/java/io/github/jairlopesjunior/sistemabibliotecario/rest/controller/ClienteController.java
@@ -3,6 +3,7 @@ package io.github.jairlopesjunior.sistemabibliotecario.rest.controller;
 import io.github.jairlopesjunior.sistemabibliotecario.domain.entities.Cliente;
 import io.github.jairlopesjunior.sistemabibliotecario.rest.dtos.ClienteDTO;
 import io.github.jairlopesjunior.sistemabibliotecario.service.ClienteService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,30 +21,35 @@ public class ClienteController {
     }
 
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Salvar um Cliente.")
     @PostMapping
     public Cliente save(@RequestBody @Valid ClienteDTO cliente){
         return clienteService.save(cliente);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiOperation("Deletar um Cliente.")
     @DeleteMapping("{id}")
     public void delete(@PathVariable Integer id){
         clienteService.delete(id);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiOperation("Atualizar um Cliente.")
     @PutMapping("{id}")
     public void update(@PathVariable Integer id, @RequestBody @Valid ClienteDTO clienteDTO){
         clienteService.update(id, clienteDTO);
     }
 
     @ResponseStatus(HttpStatus.ACCEPTED)
+    @ApiOperation("Buscar todos os Clientes.")
     @GetMapping
     public List<Cliente> getAll(){
         return clienteService.findAll();
     }
 
     @ResponseStatus(HttpStatus.ACCEPTED)
+    @ApiOperation("Buscar o Cliente pelo ID.")
     @GetMapping("{id}")
     public Cliente getById(@PathVariable Integer id){
         return clienteService.findById(id);
