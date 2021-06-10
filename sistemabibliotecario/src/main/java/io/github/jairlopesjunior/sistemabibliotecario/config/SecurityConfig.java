ec917ae5504fa35c98cd24db366c30d1a8044469
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-06-09 21:58:03 -0300

    Adicionado um metodo @Bean para o Spring gerenciar utilizando BCriptPasswordEncoder

--- sistemabibliotecario/src/main/java/io/github/jairlopesjunior/sistemabibliotecario/config/SecurityConfig.java
+++ sistemabibliotecario/src/main/java/io/github/jairlopesjunior/sistemabibliotecario/config/SecurityConfig.java
@@ -1,12 +1,20 @@
 package io.github.jairlopesjunior.sistemabibliotecario.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+       return new BCryptPasswordEncoder();
+    }
 
     //  Realiza a autenticação dos USUARIOS
     @Override
